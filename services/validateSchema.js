ddb6346da0669fb03d140966654c28072d0622c7
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-30 19:20:38 -0300

    altera função validateSchema
    adiciona o parâmetro status com valor padrão 400

--- services/validateSchema.js
+++ services/validateSchema.js
@@ -1,8 +1,8 @@
 const CustomError = require('../errors/CustomError');
 
-const validateSchema = (schema, data) => {
+const validateSchema = (schema, data, status = 400) => {
   const { error } = schema.validate(data);
-  if (error) throw new CustomError(400, error.message);
+  if (error) throw new CustomError(status, error.message);
 };
 
 module.exports = validateSchema;
