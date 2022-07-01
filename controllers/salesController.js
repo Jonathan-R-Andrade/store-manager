8bc924daac66b0be665fd7d41fd27cd6f7d97c91
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-01 00:56:20 -0300

    adiciona validação de produtos
    validação dos produtos adicionados em uma venda

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,6 +1,7 @@
 const salesService = require('../services/salesService');
 
 const addSale = async (req, res) => {
+  salesService.validateProducts(req.body);
   const result = await salesService.addSale(req.body);
   res.status(201).json(result);
 };
--- services/salesService.js
+++ services/salesService.js
@@ -1,5 +1,24 @@
+const Joi = require('joi');
 const salesModel = require('../models/salesModel');
 const salesProductsModel = require('../models/salesProductsModel');
+const validateSchema = require('./validateSchema');
+
+const validateProducts = (products) => {
+  const schemaRequired = Joi.object({
+    productId: Joi.number().required(),
+    quantity: Joi.number().required(),
+  }).label('products');
+
+  const schemaMin = Joi.object({
+    productId: Joi.number().min(1),
+    quantity: Joi.number().min(1),
+  }).label('products');
+
+  products.forEach((product) => {
+    validateSchema(schemaRequired, product, 400);
+    validateSchema(schemaMin, product, 422);
+  });
+};
 
 const addSale = async (products) => {
   const saleId = await salesModel.addSale();
@@ -7,4 +26,4 @@ const addSale = async (products) => {
   return { id: saleId, itemsSold: products };
 };
 
-module.exports = { addSale };
+module.exports = { validateProducts, addSale };
