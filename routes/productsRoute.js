4289ac746ddfe7ab0e227ace5a37465d6bb2a087
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-01 16:43:19 -0300

    cria rota
    cria rota para atualizar um produto

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -6,5 +6,6 @@ const productsRouter = Router();
 productsRouter.get('/', productsController.listProducts);
 productsRouter.get('/:id', productsController.getProduct);
 productsRouter.post('/', productsController.addProduct);
+productsRouter.put('/:id', productsController.updateProduct);
 
 module.exports = productsRouter;
