dfd9ee81e6ab4f80e5ea899e8524e95dc0e8db93
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-01 15:13:56 -0300

    cria rotas
    cria rota para listar vendas com produtos e rota para obter produtos
    de uma venda específica

--- routes/salesRoute.js
+++ routes/salesRoute.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const salesController = require('../controllers/salesController');
+const salesProductsController = require('../controllers/salesProductsController');
 
 const salesRouter = Router();
 
+salesRouter.get('/', salesProductsController.listSalesWithProducts);
+salesRouter.get('/:id', salesProductsController.getProductsFromASale);
 salesRouter.post('/', salesController.addSale);
 
 module.exports = salesRouter;
