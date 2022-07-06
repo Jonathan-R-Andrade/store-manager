5f144ea7fa77a7024587cc318437e53299b231fe
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-06 20:19:44 -0300

    cria testes
    cria testes para as funções de validações

--- tests/unit/services/validations.test.js
+++ tests/unit/services/validations.test.js
@@ -1,8 +1,16 @@
-const { expect } = require('chai');
+const { expect, use } = require('chai');
 const CustomError = require('../../../errors/CustomError');
 const validations = require('../../../services/validations');
+const productsModel = require('../../../models/productsModel');
+const salesProductsModel = require('../../../models/salesProductsModel');
+const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+
+use(chaiAsPromised);
 
 describe('validations', () => {
+
+  afterEach(sinon.restore);
 
   describe('#validateId', () => {
 
@@ -16,6 +24,50 @@ describe('validations', () => {
     describe('ao receber um id valido', async () => {
       it('não lança exceção', async () => {
         expect(() => validations.validateId(1)).to.not.throw(Error);
+      });
+    });
+
+  });
+
+  describe('#validateIfProductsExist', () => {
+
+    describe('se algum produto não existe', async () => {
+      it('lança uma exceção com a mensagem "Product not found"', async () => {
+        sinon.stub(productsModel, 'countFoundProducts').resolves(1);
+
+        await expect(validations.validateIfProductsExist([1, 2]))
+          .to.be.rejectedWith(CustomError, 'Product not found');
+      });
+    });
+
+    describe('se todos os produtos existem', async () => {
+      it('não lança exceção', async () => {
+        sinon.stub(productsModel, 'countFoundProducts').resolves(2);
+
+        await expect(validations.validateIfProductsExist([1, 2]))
+          .not.to.be.rejectedWith(Error);
+      });
+    });
+
+  });
+
+  describe('#validateIfExistsSaleOfProducts', () => {
+
+    describe('se a venda não existe', async () => {
+      it('lança uma exceção com a mensagem "Sale not found"', async () => {
+        sinon.stub(salesProductsModel, 'checkIfExistsSaleOfProducts').resolves(false);
+
+        await expect(validations.validateIfExistsSaleOfProducts(999))
+          .to.be.rejectedWith(CustomError, 'Sale not found');
+      });
+    });
+
+    describe('se a venda existe', async () => {
+      it('não lança exceção', async () => {
+        sinon.stub(salesProductsModel, 'checkIfExistsSaleOfProducts').resolves(true);
+
+        await expect(validations.validateIfExistsSaleOfProducts(1))
+          .not.to.be.rejectedWith(Error);
       });
     });
 
