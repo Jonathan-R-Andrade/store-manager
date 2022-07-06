a5e04191f94a5299e3ddaa3e47c1dd6734669180
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-06 01:11:38 -0300

    cria rota
    cria rota para pesquisar produtos por termo

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -4,6 +4,7 @@ const productsController = require('../controllers/productsController');
 const productsRouter = Router();
 
 productsRouter.get('/', productsController.listProducts);
+productsRouter.get('/search/', productsController.getProductsBySearchTerm);
 productsRouter.get('/:id', productsController.getProduct);
 productsRouter.post('/', productsController.addProduct);
 productsRouter.put('/:id', productsController.updateProduct);
