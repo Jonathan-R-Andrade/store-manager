9f86440c4973de240e00adaeb12acf69f5394364
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-07-10 18:53:23 -0300

    refactor: altera o arquivo de queries "sqlQueries.js"
    Alteração feita para usar o nome do banco de dados na variável de
    ambiente.

--- src/models/sqlQueries.js
+++ src/models/sqlQueries.js
@@ -1,55 +1,63 @@
+require('../utils/dotEnvVariables').load();
+
+// Returns a string of question marks separated by commas,
+// ex: makeQuestionMarks(3) returns '?,?,?'
 const makeQuestionMarks = (total) => new Array(total).fill('?').join(',');
-const makeRows = (totalRows, totalColumns) => new Array(totalRows)
-  .fill(`(${makeQuestionMarks(totalColumns)})`).join(',');
+// Returns a string of question marks separated by commas within parentheses
+// separated by commas, ex: makeRows(3, 2) returns '(?,?),(?,?),(?,?)'
+const makeRows = (totalRows, totalColumns) =>
+  new Array(totalRows).fill(`(${makeQuestionMarks(totalColumns)})`).join(',');
+
+const dbName = process.env.MYSQL_DATABASE;
 
 const sqlQueries = {
   // products queries
-  listProducts: () => 'SELECT * FROM StoreManager.products',
-  getProduct: () => 'SELECT * FROM StoreManager.products WHERE id=?',
-  addProduct: () => 'INSERT INTO StoreManager.products (name) VALUES (?)',
-  updateProduct: () => 'UPDATE StoreManager.products SET name=? WHERE id=?',
-  deleteProduct: () => 'DELETE FROM StoreManager.products WHERE id=?',
+  listProducts: () => `SELECT * FROM ${dbName}.products`,
+  getProduct: () => `SELECT * FROM ${dbName}.products WHERE id=?`,
+  addProduct: () => `INSERT INTO ${dbName}.products (name) VALUES (?)`,
+  updateProduct: () => `UPDATE ${dbName}.products SET name=? WHERE id=?`,
+  deleteProduct: () => `DELETE FROM ${dbName}.products WHERE id=?`,
   countFoundProducts: (totalIds) => `
-    SELECT count('products') AS foundProducts FROM StoreManager.products
+    SELECT count('products') AS foundProducts FROM ${dbName}.products
     WHERE id in (${makeQuestionMarks(totalIds)});
   `,
   getProductsBySearchTerm: () => `
-    SELECT * FROM StoreManager.products
+    SELECT * FROM ${dbName}.products
     WHERE name LIKE ?;
   `,
 
   // sales queries
-  addSale: () => 'INSERT INTO StoreManager.sales VALUES ()',
-  deleteSale: () => 'DELETE FROM StoreManager.sales WHERE id=?',
+  addSale: () => `INSERT INTO ${dbName}.sales VALUES ()`,
+  deleteSale: () => `DELETE FROM ${dbName}.sales WHERE id=?`,
 
   // product_id queries
   addSaleProducts: (totalRows, totalColumns) => `
-    INSERT INTO StoreManager.sales_products
+    INSERT INTO ${dbName}.sales_products
     VALUES ${makeRows(totalRows, totalColumns)}
   `,
   listSalesWithProducts: () => `
     SELECT s.id AS saleId, s.date, p.id AS productId, sp.quantity
-    FROM StoreManager.sales_products AS sp
-    INNER JOIN StoreManager.products AS p
-    INNER JOIN StoreManager.sales AS s
+    FROM ${dbName}.sales_products AS sp
+    INNER JOIN ${dbName}.products AS p
+    INNER JOIN ${dbName}.sales AS s
     ON sp.sale_id = s.id AND sp.product_id = p.id
     ORDER BY s.id ASC, p.id ASC;
   `,
   getProductsFromASale: () => `
     SELECT s.date, p.id AS productId, sp.quantity
-    FROM StoreManager.sales_products AS sp
-    INNER JOIN StoreManager.products AS p
-    INNER JOIN StoreManager.sales AS s
+    FROM ${dbName}.sales_products AS sp
+    INNER JOIN ${dbName}.products AS p
+    INNER JOIN ${dbName}.sales AS s
     ON sp.sale_id = s.id AND sp.product_id = p.id
     WHERE s.id = ?;
   `,
   updateProductFromASale: () => `
-    UPDATE StoreManager.sales_products
+    UPDATE ${dbName}.sales_products
     SET quantity=?
     WHERE sale_id=? AND product_id=?;
   `,
   checkIfExistsSaleOfProducts: () => `
-    SELECT count('exists') AS \`exists\` FROM StoreManager.sales_products
+    SELECT count('exists') AS \`exists\` FROM ${dbName}.sales_products
     WHERE sale_id=?;
   `,
 };
