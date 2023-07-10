017e5ca680fd14e1c906fb1251a13a27c2b7faf4
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-07-10 18:40:49 -0300

    refactor: altera configuração das variáveis de ambiente

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
-const app = require('./app');
+require('./utils/dotEnvVariables').load();
 
-// não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+const app = require('./app');
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
--- src/models/connection.js
+++ src/models/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '',
-  database: process.env.MYSQL_DATABASE || 'StoreManager',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
